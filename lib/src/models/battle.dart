614cc6b6f883cfef8b743d6ad94ba79242db3213
winche <daniellastra@winche.com>
2019-03-11 18:08:08 +0100

    Initial commit

---
+++ lib/src/models/battle.dart
@@ -0,0 +1,12 @@
+import 'package:fms_app/src/models/mc.dart';
+
+class Battle{
+
+  List<Mc> mcs;
+  int duration;
+  Mc winner;
+  String location;
+  String videoUrl;
+
+
+}
---
+++ lib/src/models/mc.dart
@@ -0,0 +1,11 @@
+class Mc {
+  String name;
+  String nickName;
+  int age;
+  String posterPath;
+  String bio;
+
+  Mc(this.name, this.nickName, this.age, this.posterPath, this.bio);
+
+
+}
